ce6379bf6d5e2958121c3a623552229f4da61f68
Joe Bruno <joevbruno@me.com>
2016-11-06 00:39:43 -0400

    updated webpack conifg

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = validate({
                 },
                 {
                     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                    loader: "file?name=./../docs/[name].[ext]"
+                    loader: "file?name=./../[name].[ext]"
                 },
 
                 {
